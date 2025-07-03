8f91098772f9d1f31c3f7a0233885518ae2755b3
DrVickie <116500226+drvickie@users.noreply.github.com>
2025-07-03 18:52:45 +0100

    noImplicitAny true/false

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ let sales: number = 123_456_789;
 let course: string = "TypeScript";
 let level: any =true;
 
-function render(document:any){
+function render(document){
     console.log(document);
 }
